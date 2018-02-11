8cc30eb907e726850df9cee6d38153ca2d59757b
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-12 12:45:10 +1300

    Bulked up ImageList markup and code

--- GeoPhotoProjec/app/components/imagesList-component/imagesList.component.ts
+++ GeoPhotoProjec/app/components/imagesList-component/imagesList.component.ts
@@ -1,5 +1,12 @@
 import {Component, NgZone} from "@angular/core";
 import {Router} from "@angular/router";
+import {Config} from "../../config";
+
+import {FlickrService} from "../services/flickr.service";
+import {GeoLocationService} from "../services/geolocation.service";
+
+import {PhotoSearchResponse} from "../models/photoSearchResponse";
+
 
 @Component({
   selector: "ImagesListComponent",
@@ -7,5 +14,60 @@ import {Router} from "@angular/router";
 })
 
 export class ImagesListComponent {
-  public constructor(private zone:NgZone, private router: Router){ }
+
+  private mapbox: any;
+  public mapboxKey: string;
+  public photos: PhotoSearchResponse[];
+
+  public constructor(private flickrService:FlickrService, private geolocationService: GeoLocationService, private zone:NgZone, private router: Router){ }
+
+  public onMapReady(args){
+    this.mapbox = args.map;
+    this.geolocationService.getLocation().then(() => {
+      this.loadPhotos().subscribe(photos => {
+        this.photos = photos.map((photo) => {
+          photo.distance = this.geolocationService.getDistanceFrom(parseFloat(photo.latitude), parseFloat(photo.longitude));
+          return photo;
+        });
+        this.dropMarkers();
+        this.mapbox.setCenter({
+          lat: this.geolocationService.latitude,
+          lng: this.geolocationService.longitude,
+          animated: true
+        });
+      },
+      error => console.log(error));
+    });
+  }
+
+  public dropMarkers(){
+    let markers = this.photos.map((photo: PhotoSearchResponse, index: number) => {
+      return {
+        lat: photo.latitude,
+        lng: photo.longitude,
+        onTop: () => {this.zone.run(() => {this.showPhoto({index: index});});}
+      }
+    });
+
+    this.mapbox.addMarkers(markers);
+  }
+
+  public centerMap(args: any){
+    let photo = this.photos[args.index];
+    this.mapbox.setCenter({
+      lat: parseFloat(photo.latitude),
+      lng: parseFloat(photo.longitude),
+      animated: false
+    });
+  }
+
+  public showPhoto(args: any){
+    let photo = this.photos[args.index];
+    this.router.navigate(["/image-component", photo.id]);
+  }
+
+  public loadPhotos(){
+    return this.flickrService.photoSearch(this.geolocationService.latitude, this.geolocationService.longitude);
+  }
+
 }
