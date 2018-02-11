45aa13faf4f3ce057e3ba3a43b7b1f8f4a058e4b
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-12 12:50:18 +1300

    Bulked up main Image display and code

--- GeoPhotoProjec/app/components/image-component/image.component.ts
+++ GeoPhotoProjec/app/components/image-component/image.component.ts
@@ -1,14 +1,29 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 
+import {FlickrService} from "../services/flickr.service";
+
 @Component({
   templateUrl: "components/image-component/image.component.html"
 })
 
 export class ImageComponent implements OnInit{
 
-  public constructor(private activatedRoute: ActivatedRoute){}
+  public url: string;
 
-  public ngOnInit(){}
-  
+  public constructor(private activatedRoute: ActivatedRoute, private flickrService: FlickrService){}
+
+  public getPhoto(photoId:number){
+    this.flickrService.getPhotoInfo(photoId).subscribe(photo => {
+      this.url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_n.jpg`;
+    }, error => console.log(error));
+  }
+
+  public ngOnInit(){
+    this.activatedRoute.params.subscribe(params => {
+      let photoId = params["photo_id"];
+      this.getPhoto(photoId);
+    });
+  }
+
 }
