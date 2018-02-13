527937b9afd51edcefea847b9d2f6dc90e4ceefe
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-14 08:21:39 +1300

    Still Object view error

--- Groceries/platforms/android/app/build/intermediates/assets/F0/debug/app/pages/list/list.component.ts
+++ Groceries/platforms/android/app/build/intermediates/assets/F0/debug/app/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild, NgZone } from "@angular/core";
 
 import {TextField} from "ui/text-field";
 
@@ -22,7 +22,7 @@ export class ListComponent implements OnInit {
   groceryList: Array<Grocery> = [];
   @ViewChild("groceryTextField") groceryTextField: ElementRef;
 
-  constructor(private groceryListService: GroceryListService){}
+  constructor(private groceryListService: GroceryListService, private zone: NgZone){ }
 
   ngOnInit() {
     this.isLoading = true;
@@ -59,6 +59,16 @@ export class ListComponent implements OnInit {
   share(){
     let listString = this.groceryList.map(grocery => grocery.name).join(", ").trim();
     SocialShare.shareText(listString);
+  }
+
+  delete(grocery: Grocery){
+    this.groceryListService.delete(grocery.id).subscribe(() => {
+      // Running the array splice in a zone ensures that change detection gets triggered.
+      this.zone.run(() => {
+        let index = this.groceryList.indexOf(grocery);
+        this.groceryList.splice(index, 1);
+      });
+    });
   }
 
 }
